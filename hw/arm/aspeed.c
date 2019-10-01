81187650cce746abba0a654420cb30696f99399c
rhm <44321907+rhmcruiser@users.noreply.github.com>
2019-10-01 18:07:26 +1000

    added slave device init for dps310 in witherspoon_bmc_i2c_init( )

--- hw/arm/aspeed.c
+++ hw/arm/aspeed.c
@@ -296,6 +296,7 @@ static void witherspoon_bmc_i2c_init(AspeedBoardState *bmc)
     uint8_t *eeprom_buf = g_malloc0(8 * 1024);
 
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 3), "pca9552", 0x60);
+    i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 3), "dps310", 0x76);
 
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 4), "tmp423", 0x4c);
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 5), "tmp423", 0x4c);
