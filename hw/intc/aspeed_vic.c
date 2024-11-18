545d7bbbe7462b80a497bcb10f769ffab43a8af8
Joel Stanley <joel@jms.id.au>
2024-11-18 12:48:19 +1030

    hw/aspeed: Correct minimum access size for all models
    Guest code was performing a byte load to the SCU MMIO region, leading to
    the guest code crashing (it should be using proper accessors, but
    that is not Qemu's bug). Hardware and the documentation[1] both agree that
    byte loads are okay, so change all of the aspeed devices to accept a
    minimum access size of 1.
    
    [1] See the 'ARM Address Space Mapping' table in the ASPEED docs. This
    is section 6.1 in the ast2400 and ast2700, and 7.1 in the ast2500 and
    ast2600 datasheets.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2636
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Reviewed-by: Troy Lee <leetroy@gmail.com>

--- hw/intc/aspeed_vic.c
+++ hw/intc/aspeed_vic.c
@@ -286,7 +286,7 @@ static const MemoryRegionOps aspeed_vic_ops = {
     .read = aspeed_vic_read,
     .write = aspeed_vic_write,
     .endianness = DEVICE_LITTLE_ENDIAN,
-    .valid.min_access_size = 4,
+    .valid.min_access_size = 1,
     .valid.max_access_size = 4,
     .valid.unaligned = false,
 };
